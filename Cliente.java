8f81b26140af84a6dffc9d03190e5f501e618200
Lívia Izidório <liviaizidorio.li@gmail.com>
2024-03-18 08:51:55 -0300

    Mudando a Classe Cliente

--- Cliente.java
+++ Cliente.java
@@ -1,7 +1,8 @@
 public class Cliente {
-    private String tipoConta;
+    private String Continha;
     private String nome;
     private String cpf;
     private int agencia;
     private double saldo;
+    
 }
